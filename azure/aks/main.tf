f4ce901ae11bd1f1ebeadd9bf1910925223fc6e4
Ubuntu <ubuntu@ip-172-31-29-23.ec2.internal>
2026-06-07 18:00:32 +0000

    ACR

--- azure/aks/main.tf
+++ azure/aks/main.tf
@@ -26,3 +26,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   tags = local.tags
 }
+
+data "azurerm_container_registry" "acr" {
+  name                = "nextopsacrdemo"
+  resource_group_name = "NexTOps-RCS"
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+
+  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
